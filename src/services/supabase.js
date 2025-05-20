fb67b80e4bcc049e6afa814135f6cdbee606aa1d
Stanislau Lihodievski <s.lihodievski@richbrains.net>
2025-05-21 00:51:34 +0300

    styling and set supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://rlpfndbvnmzfslkhsidd.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJscGZuZGJ2bm16ZnNsa2hzaWRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NzI4OTcsImV4cCI6MjA2MzM0ODg5N30.qoDurBZSxfwBJS5cYNuMXsVQ2gochj53NwQobSLXPgc";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
